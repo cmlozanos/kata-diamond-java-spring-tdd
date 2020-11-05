7b545f45048976309fbbc941e0c71bd58a9587bc
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-05 16:13:44 +0100

    fix test for index of letter at Diamond Service

--- src/main/java/com/example/katadiamondjavaspringtdd/services/DiamondServiceImpl.java
+++ src/main/java/com/example/katadiamondjavaspringtdd/services/DiamondServiceImpl.java
@@ -9,6 +9,8 @@ import com.example.katadiamondjavaspringtdd.web.controller.Diamond;
 @Service
 public class DiamondServiceImpl implements DiamondService {
 
+	private static final String LETTERS = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
 	@Override
 	public Diamond of(final String letter) {
 		if ("A".equalsIgnoreCase(letter)) {
@@ -18,7 +20,6 @@ public class DiamondServiceImpl implements DiamondService {
 	}
 
 	public int indexOfLetter(final String letter) {
-		// TODO Auto-generated method stub
-		return 0;
+		return DiamondServiceImpl.LETTERS.indexOf(letter) + 1;
 	}
 }
