f14874f2a9afdeca1fcc957c634b8bbbe0c6d850
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-05 15:25:54 +0100

    add test for diamond service

---
+++ src/main/java/com/example/katadiamondjavaspringtdd/services/DiamondService.java
@@ -0,0 +1,9 @@
+package com.example.katadiamondjavaspringtdd.services;
+
+import com.example.katadiamondjavaspringtdd.web.controller.Diamond;
+
+public interface DiamondService {
+
+	Diamond of(String letter);
+
+}
---
+++ src/test/java/com/example/katadiamondjavaspringtdd/services/DiamondServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.katadiamondjavaspringtdd.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.katadiamondjavaspringtdd.web.controller.Diamond;
+
+@SpringBootTest
+public class DiamondServiceTest {
+
+	@Autowired
+	DiamondService service;
+
+	@Test
+	public void givenLetterBWhenCreateDiamondThenShouldReturnDiamondOfB() {
+		// given
+
+		// when
+		final Diamond diamond = this.service.of("B");
+
+		// then
+		Assertions.assertNotNull(diamond);
+		Assertions.assertNotNull(diamond.getRows());
+		Assertions.assertEquals(3, diamond.getRows().size());
+		Assertions.assertEquals(" A ", diamond.getRows().get(0));
+		Assertions.assertEquals("B B", diamond.getRows().get(0));
+		Assertions.assertEquals(" A ", diamond.getRows().get(0));
+	}
+
+}
