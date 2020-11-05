eadd9324f8ae7f0f1df18b5a41eeec2cf9370212
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-05 15:40:46 +0100

    fix test for diamond service

---
+++ src/main/java/com/example/katadiamondjavaspringtdd/services/DiamondServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.katadiamondjavaspringtdd.services;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import com.example.katadiamondjavaspringtdd.web.controller.Diamond;
+
+@Service
+public class DiamondServiceImpl implements DiamondService {
+
+	@Override
+	public Diamond of(final String letter) {
+		if ("A".equalsIgnoreCase(letter)) {
+			return Diamond.builder().rows(Arrays.asList(" A ")).build();
+		}
+		return Diamond.builder().rows(Arrays.asList(" A ", "B B", " A ")).build();
+	}
+}
