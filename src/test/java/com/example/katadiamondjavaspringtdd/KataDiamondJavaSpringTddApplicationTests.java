d6635d726b5445f124b8f4efbad232f23c82fe64
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-05 10:07:34 +0100

    initial commit

---
+++ src/test/java/com/example/katadiamondjavaspringtdd/KataDiamondJavaSpringTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katadiamondjavaspringtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataDiamondJavaSpringTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
